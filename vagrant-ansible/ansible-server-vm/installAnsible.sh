ef6d49ace0bf320529b074458a6373c6a4fc8b9a
Luiz Henrique <luizhmc@gmail.com>
2014-12-08 11:53:28 -0300

    ajustes para instalar java

--- vagrant-ansible/ansible-server-vm/installAnsible.sh
+++ vagrant-ansible/ansible-server-vm/installAnsible.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 
 if [ ! -f /usr/bin/ansible ]; then
   echo "Installing Ansible and dependencies."
-	sudo apt-get --no-install-recommends -q -y install ansible sshpass
+	#sudo apt-get --no-install-recommends -q -y install ansible sshpass
+	sudo apt-get install python-pip python-dev git make sshpass -y
+	pip install PyYAML jinja2 paramiko ansible
   echo "Ansible installed."
 fi 
